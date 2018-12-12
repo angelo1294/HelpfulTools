869b98e801914f29c671891defc2fdf6f3d1d59c
Grama Andrei <andrei.grama@spirent.com>
2018-12-12 18:51:57 +0200

    Modified initialize to receive parameters

--- initialize.sh
+++ initialize.sh
@@ -6,24 +6,74 @@ do
 key="$1"
 
 case $key in
-    -v|--velocity)
-    EXTENSION="$2"
+    -vcenter|--vcenter)
+    vcenter="$2"
     shift # past argument
     shift # past value
     ;;
+    -vel|--vel)
+    vel="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -ite|--ite)
+    ite="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -version|--version)
+    version="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -user|--user)
+    user="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -password|--password)
+    password="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
 esac
 done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
+###Replace said char with - ###
+for var in {vcenter vel ite}; do
+	$(echo $var) = tr - \\- 
 
-libsList=("pyyaml" "requests" "argparse" "psutil")
-for lib in ${libsList[@]}; do
-	pip install $lib
-done
-if ( (service velocity-agent status)>0 )
-then
-	echo "Agent is ok"
-else
-	chmod +x agent_service.sh
-	echo "yes" | ./agent_service.sh -v ${VELOCITY}
-fi
+###Install libraries###
+# libsList=("pyyaml" "requests" "argparse" "psutil")
+# for lib in ${libsList[@]}; do
+# 	pip install $lib
+# done
+
+###Install powershell###
+# if (! pswh)
+# then
+# 	wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+# 	sudo dpkg -i packages-microsoft-prod.deb
+# 	sudo apt-get update | echo Y
+# 	sudo apt-get install -y powershell
+# fi
+###Execute script in powershell###
+pwsh changeBuild.sh -vcenter $vcenter -version $version -user $user -password $password -ite $ite -vel $vel
+###Check agent service###
+# if ( (service velocity-agent status)>0 )
+# then
+# 	echo "Agent is ok"
+# else
+# 	chmod +x agent_service.sh
+# 	echo "yes" | ./agent_service.sh -v ${v}
+# fi
 
